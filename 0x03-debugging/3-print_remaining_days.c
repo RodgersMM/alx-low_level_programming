21be614d02eed4577d98721d94cfde605941f078
RodgersMM <rodgersmutua44@gmail.com>
2023-07-20 07:22:49 -0700

    print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_remaining_days - takes a date and prints how many days are
@@ -7,7 +7,7 @@
  * @month: month in number format
  * @day: day of month
  * @year: year
- * Return: void
+ * Return: 0
  */
 
 void print_remaining_days(int month, int day, int year)
