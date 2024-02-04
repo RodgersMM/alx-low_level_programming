60b9581b23d97f6dd938b3cba99136798e6a5bfb
RodgersMM <rodgers>
2024-02-03 23:09:06 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/RodgersMM/alx-low_level_programming/main/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -p .. https://raw.githubusercontent.com/RodgersMM/alx-low_level_programming/main/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
